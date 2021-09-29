c43bf861ce51162278071f3f1422cbf18d66600d
Fuso-wasedr09 <45784978+fusowasedr09@users.noreply.github.com>
2021-09-29 22:14:17 +0900

    Add files via upload

---
+++ livesplit-discordbot.py
@@ -0,0 +1,38 @@
+import discord, subprocess, socket
+token="ここにアクセストークンを入れる"
+client = discord.Client()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+@client.event
+async def on_ready():
+    s.connect(("localhost", 16834))
+    print("ログインしました")
+    await message.channel.send("ログインしました")
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "!start":
+        s.send(b"starttimer\r\n")
+        await message.channel.send("タイマースタート！")
+    if message.content == "!split":
+        s.send(b"split\r\n")
+        await message.channel.send("次の区間に進みます")
+    if message.content == "!reset":
+        s.send(b"reset\r\n")
+        await message.channel.send("タイマーをリセットしました")
+    if message.content == "!pause":
+        s.send(b"pause\r\n")
+        await message.channel.send("タイマーを一時停止しました")
+    if message.content == "!resume":
+        s.send(b"resume\r\n")
+        await message.channel.send("タイマーを再開します")
+    if message.content == "!back":
+        s.send(b"unsplit\r\n")
+        await message.channel.send("前の区間に戻します")
+    if message.content == "!skip":
+        s.send(b"skip\r\n")
+        await message.channel.send("この区間をスキップします")
+
+client.run(token)
